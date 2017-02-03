4db5aa2f95917e198dc1c165c60e315ab376f911
WILL YANG LI <liyang.pku.86@gmail.com>
2017-02-03 14:31:59 -0600

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,55 @@
+library(plyr)
+
+# Download raw data
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+
+unzip(zipfile ="./data/Dataset.zip",exdir="./data")
+files <- list.files(path_rf, recursive=TRUE)
+
+#extract sub files
+dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+
+#combine training and testing dataset
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity <- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures <- rbind(dataFeaturesTrain, dataFeaturesTest)
+
+#update combined dataset's col name
+names(dataSubject) <- c("subject")
+names(dataActivity) <- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures) <- dataFeaturesNames$V2
+
+#recombine subject, activity, features
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+
+#Select col with mean and std in the name file, and reformat names of features and activity
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames <-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data <-subset(Data,select=selectedNames)
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+Data$activity <- factor(Data$activity,labels=activityLabels$V2)
+
+#reformat names by replacing abbreviated expressions
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+names(Data)
+
+#aggregte data by mean with combination of subject and activity, and reorder by categories
+Data2 <- aggregate(. ~subject + activity, Data, mean)
+Data2 <- Data2[order(Data2$subject, Data2$activity),]
+
+#generate tidydata.txt
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
